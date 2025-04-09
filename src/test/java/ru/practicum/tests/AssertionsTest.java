9c1d78434ed8e0d305659e47bc6b3a8a88a56dd1
Aleksandr Shipovalov <ashipovalov@gmail.com>
2025-04-09 20:09:33 +0300

    Добавлены примеры основных Assertions

---
+++ src/test/java/ru/practicum/tests/AssertionsTest.java
@@ -0,0 +1,34 @@
+package ru.practicum.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertionsTest {
+
+
+    @Test
+    void standartAssertions(){
+        assertEquals(2, 1+1);
+        assertTrue(6 > 4, "Или нет");
+        assertNull(null);
+
+    }
+
+    @Test
+    void groupedAssertions(){
+        String name = "John Doe";
+        assertAll("person",
+                () -> assertEquals("John", name.split(" ")[0]),
+                () -> assertEquals("Doe", name.split(" ")[1]));
+    }
+
+    @Test
+    void exceptionTesting(){
+        Exception exception = assertThrows(
+        ArithmeticException.class,
+                () -> {int i = 1 /0; }
+        );
+        assertEquals("/ by zero" , exception.getMessage());
+    }
+}
